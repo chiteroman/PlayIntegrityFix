bcaa4327b91d6c7e28e31ca420eba62b9474b8bb
chiteroman <98092901+chiteroman@users.noreply.github.com>
2023-11-27 19:09:13 +0100

    Add module files

---
+++ module/customize.sh
@@ -0,0 +1,10 @@
+# Error on < Android 8
+if [ "$API" -lt 26 ]; then
+    abort "!!! You can't use this module on Android < 8.0."
+fi
+
+# safetynet-fix module is incompatible
+if [ -d "/data/adb/modules/safetynet-fix" ]; then
+    touch "/data/adb/modules/safetynet-fix/remove"
+	ui_print "- 'safetynet-fix' module will be removed in next reboot."
+fi
