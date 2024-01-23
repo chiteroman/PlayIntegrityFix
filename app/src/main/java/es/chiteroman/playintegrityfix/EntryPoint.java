e654d2f0ef4f24b0115248aaef70b113167cddc7
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-01-23 18:54:10 +0300

    Update EntryPoint.java

--- app/src/main/java/es/chiteroman/playintegrityfix/EntryPoint.java
+++ app/src/main/java/es/chiteroman/playintegrityfix/EntryPoint.java
@@ -15,69 +15,55 @@ import java.util.Map;
 
 public final class EntryPoint {
     private static final Map<Field, String> map = new HashMap<>();
+    private static final Map<String, Field> fieldCache = new HashMap<>();
+    private static JSONObject jsonObject;
 
-    public static void init(String json) {
-
-        spoofProvider();
-
+    static {
         try {
-            JSONObject jsonObject = new JSONObject(json);
-
-            jsonObject.keys().forEachRemaining(s -> {
-                try {
-                    String value = jsonObject.getString(s);
-                    Field field = getFieldByName(s);
-
-                    if (field == null) {
-                        LOG("Field " + s + " not found!");
-                        return;
-                    }
-
-                    map.put(field, value);
-                    LOG("Save " + field.getName() + " with value: " + value);
-
-                } catch (Throwable t) {
-                    LOG("Couldn't parse " + s + " key!");
-                }
-            });
-
-            LOG("Map size: " + map.size());
-            spoofFields();
-
-        } catch (Throwable t) {
-            LOG("Error loading json file: " + t);
-        }
-    }
-
-    private static void spoofProvider() {
-        try {
-            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
-            keyStore.load(null);
-
-            Field f = keyStore.getClass().getDeclaredField("keyStoreSpi");
-
-            f.setAccessible(true);
-            CustomKeyStoreSpi.keyStoreSpi = (KeyStoreSpi) f.get(keyStore);
-            f.setAccessible(false);
-
             Provider provider = Security.getProvider("AndroidKeyStore");
-
             Provider customProvider = new CustomProvider(provider);
-
             Security.removeProvider("AndroidKeyStore");
             Security.insertProviderAt(customProvider, 1);
-
             LOG("Spoof Provider done!");
-
         } catch (Throwable t) {
             LOG("Error trying to spoof Provider: " + t);
         }
     }
 
+    public static void init(String json) {
+        try {
+            jsonObject = new JSONObject(json);
+            spoofFields();
+        } catch (Throwable t) {
+            LOG("Error loading json file: " + t);
+        }
+    }
+
     public static void spoofFields() {
+        jsonObject.keys().forEachRemaining(s -> {
+            try {
+                String value = jsonObject.getString(s);
+                Field field = getFieldByName(s);
+
+                if (field == null) {
+                    LOG("Field " + s + " not found!");
+                    return;
+                }
+
+                if (value.equals(field.get(null))) return;
+
+                map.put(field, value);
+                LOG("Save " + field.getName() + " with value: " + value);
+
+            } catch (Throwable t) {
+                LOG("Couldn't parse " + s + " key!");
+            }
+        });
+
+        LOG("Map size: " + map.size());
+
         map.forEach((field, s) -> {
             try {
-                if (s.equals(field.get(null))) return;
                 field.set(null, s);
                 LOG("Set " + field.getName() + " field value: " + s);
             } catch (IllegalAccessException e) {
@@ -87,6 +73,9 @@ public final class EntryPoint {
     }
 
     private static Field getFieldByName(String name) {
+        if (fieldCache.containsKey(name)) {
+            return fieldCache.get(name);
+        }
 
         Field field;
         try {
@@ -95,12 +84,13 @@ public final class EntryPoint {
             try {
                 field = Build.VERSION.class.getDeclaredField(name);
             } catch (NoSuchFieldException ex) {
+                LOG("Field " + name + " not found!");
                 return null;
             }
         }
 
         field.setAccessible(true);
-
+        fieldCache.put(name, field);
         return field;
     }
 
