12ccd543e4551d0dd71f062dd24836797b78bd46
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-01-23 21:05:31 +0300

    Update EntryPoint.java

--- app/src/main/java/es/chiteroman/playintegrityfix/EntryPoint.java
+++ app/src/main/java/es/chiteroman/playintegrityfix/EntryPoint.java
@@ -6,76 +6,74 @@ import android.util.Log;
 import org.json.JSONObject;
 
 import java.lang.reflect.Field;
-import java.security.KeyStore;
-import java.security.KeyStoreSpi;
 import java.security.Provider;
 import java.security.Security;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class EntryPoint {
-    private static final Map<Field, String> map = new HashMap<>();
-    private static final Map<String, Field> fieldCache = new HashMap<>();
-    private static JSONObject jsonObject;
-
-    static {
-        try {
-            Provider provider = Security.getProvider("AndroidKeyStore");
-            Provider customProvider = new CustomProvider(provider);
-            Security.removeProvider("AndroidKeyStore");
-            Security.insertProviderAt(customProvider, 1);
-            LOG("Spoof Provider done!");
-        } catch (Throwable t) {
-            LOG("Error trying to spoof Provider: " + t);
-        }
-    }
+    private static final Map<String, String> map = new HashMap<>();
 
     public static void init(String json) {
+
+        spoofProvider();
+
         try {
-            jsonObject = new JSONObject(json);
+            JSONObject jsonObject = new JSONObject(json);
+
+            jsonObject.keys().forEachRemaining(s -> {
+                try {
+                    String value = jsonObject.getString(s);
+                    map.put(s, value);
+                    LOG("Save " + s + " with value: " + value);
+
+                } catch (Throwable t) {
+                    LOG("Couldn't parse " + s + " key!");
+                }
+            });
+
+            LOG("Map size: " + map.size());
             spoofFields();
+
         } catch (Throwable t) {
             LOG("Error loading json file: " + t);
         }
     }
 
-    public static void spoofFields() {
-        jsonObject.keys().forEachRemaining(s -> {
-            try {
-                String value = jsonObject.getString(s);
-                Field field = getFieldByName(s);
+    private static void spoofProvider() {
+        try {
+            Provider provider = Security.getProvider("AndroidKeyStore");
+            Provider customProvider = new CustomProvider(provider);
 
+            Security.removeProvider("AndroidKeyStore");
+            Security.insertProviderAt(customProvider, 1);
+
+            LOG("Spoof Provider done!");
+
+        } catch (Throwable t) {
+            LOG("Error trying to spoof Provider: " + t);
+        }
+    }
+
+    public static void spoofFields() {
+        map.forEach((name, value) -> {
+            try {
+                Field field = getFieldByName(name);
                 if (field == null) {
-                    LOG("Field " + s + " not found!");
+                    LOG("Field " + name + " not found!");
                     return;
                 }
 
                 if (value.equals(field.get(null))) return;
-
-                map.put(field, value);
-                LOG("Save " + field.getName() + " with value: " + value);
-
-            } catch (Throwable t) {
-                LOG("Couldn't parse " + s + " key!");
-            }
-        });
-
-        LOG("Map size: " + map.size());
-
-        map.forEach((field, s) -> {
-            try {
-                field.set(null, s);
-                LOG("Set " + field.getName() + " field value: " + s);
+                field.set(null, value);
+                LOG("Set " + name + " field value: " + value);
             } catch (IllegalAccessException e) {
-                LOG("Couldn't access " + field.getName() + " value " + s + " | Exception: " + e);
+                LOG("Couldn't access " + name + " value " + value + " | Exception: " + e);
             }
         });
     }
 
     private static Field getFieldByName(String name) {
-        if (fieldCache.containsKey(name)) {
-            return fieldCache.get(name);
-        }
 
         Field field;
         try {
@@ -84,13 +82,12 @@ public final class EntryPoint {
             try {
                 field = Build.VERSION.class.getDeclaredField(name);
             } catch (NoSuchFieldException ex) {
-                LOG("Field " + name + " not found!");
                 return null;
             }
         }
 
         field.setAccessible(true);
-        fieldCache.put(name, field);
+
         return field;
     }
 
