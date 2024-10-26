721c93e7f0e414e83725edce3c3aa25d72113df3
3a06fa5cf14d569c <mawalakacoco@gmail.com>
2024-10-26 23:10:25 +0800

    Check zygisksu before execute magisk

--- module/customize.sh
+++ module/customize.sh
@@ -8,8 +8,7 @@ if ! $BOOTMODE; then
 fi
 
 # Module requires Zygisk to work
-isZygiskEnabled=$(magisk --sqlite "SELECT value FROM settings WHERE key='zygisk';")
-if [ "$isZygiskEnabled" == "value=0" ] && [ ! -d "/data/adb/modules/zygisksu" ]; then
+if [ ! -d "/data/adb/modules/zygisksu" ] && [ "$(magisk --sqlite "SELECT value FROM settings WHERE key='zygisk';")" == "value=0" ]; then
     abort "! Zygisk is not enabled. Please, enable Zygisk in Magisk settings or install ZygiskNext or ReZygisk module."
 fi
 
