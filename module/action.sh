6f1d377c362ad438fe678676145f6869322b82b8
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-12-07 17:42:04 +0300

    Update action.sh
    Possible future changes

--- module/action.sh
+++ module/action.sh
@@ -1,4 +1,6 @@
 su -c killall com.google.android.gms
 su -c killall com.google.android.gms.unstable
+su -c killall com.google.process.gapps
+su -c killall com.google.process.gservices
 
 echo "Gms and Play Integrity API restarted."
