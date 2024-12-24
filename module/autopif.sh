5e22e0f59b746d53ecbf9bda152e4f14322a2a8b
KOWX712 <leecc0503@gmail.com>
2024-12-24 15:48:43 +0800

    module/autopif: simplify
    this leaves core functionality intact
    this also ignores dev previews as stated on
    https://github.com/osm0sis/PlayIntegrityFork/commit/c3ee7e5c0acdad715639ec8de555ce468cb58f6e
    
    Co-Authored-By: Chris Renshaw <osm0sis@outlook.com>

--- module/autopif.sh
+++ module/autopif.sh
@@ -1,164 +1,93 @@
-#!/system/bin/sh
+#!/bin/sh
 
-if [ "$USER" != "root" -a "$(whoami 2>/dev/null)" != "root" ]; then
-  echo "autopif2: need root permissions";
-  exit 1;
-fi;
-
-case "$1" in
-  -h|--help|help) echo "sh autopif2.sh [-a]"; exit 0;;
-  -a|--advanced|advanced) ARGS="-a"; shift;;
-esac;
-
-echo "Pixel Beta pif.json generator script \
-  \n  by osm0sis @ xda-developers";
-
-case "$0" in
-  *.sh) DIR="$0";;
-  *) DIR="$(lsof -p $$ 2>/dev/null | grep -o '/.*autopif2.sh$')";;
-esac;
-DIR=$(dirname "$(readlink -f "$DIR")");
-
-item() { echo "\n- $@"; }
-die() { echo "\nError: $@, install busybox!"; exit 1; }
+die() { echo "Error: $@!"; exit 1; }
 
 find_busybox() {
-  [ -n "$BUSYBOX" ] && return 0;
-  local path;
-  for path in /data/adb/modules/busybox-ndk/system/*/busybox /data/adb/magisk/busybox /data/adb/ksu/bin/busybox /data/adb/ap/bin/busybox; do
-    if [ -f "$path" ]; then
-      BUSYBOX="$path";
-      return 0;
-    fi;
-  done;
-  return 1;
+    [ -n "$BUSYBOX" ] && exit 0
+    for path in /data/adb/modules/busybox-ndk/system/*/busybox /data/adb/magisk/busybox /data/adb/ksu/bin/busybox /data/adb/ap/bin/busybox; do
+        if [ -f "$path" ]; then
+            BUSYBOX="$path"
+            exit 0
+        fi
+    done
+    exit 1
 }
 
-if ! which wget >/dev/null || grep -q "wget-curl" $(which wget); then
-  if ! find_busybox; then
-    die "wget not found";
-  elif $BUSYBOX ping -c1 -s2 android.com 2>&1 | grep -q "bad address"; then
-    die "wget broken";
-  else
-    wget() { $BUSYBOX wget "$@"; }
-  fi;
-fi;
+if date -D '%s' -d "$(date '+%s')" 2>&1 | grep -qE "bad date|invalid option"; then
+    find_busybox && date() { $BUSYBOX date "$@"; } || die "date broken"
+fi
 
-if date -D '%s' -d "$(date '+%s')" 2>&1 | grep -q "bad date"; then
-  if ! find_busybox; then
-    die "date broken";
-  else
-    date() { $BUSYBOX date "$@"; }
-  fi;
-fi;
+if { ! command -v wget >/dev/null 2>&1 || grep -q "wget-curl" "$(command -v wget 2>/dev/null)"; }; then
+    find_busybox && wget() { $BUSYBOX wget "$@"; } || die "wget not found, install busybox"
+fi
 
-if [ "$DIR" = /data/adb/modules/playintegrityfix ]; then
-  DIR=$DIR/autopif2;
-  mkdir -p $DIR;
-fi;
-cd "$DIR";
+if echo -e "A\nB" | grep -q "A.*B"; then
+    find_busybox || die "grep broken"
+    grep() { $BUSYBOX grep "$@"; }
+fi
 
-item "Crawling Android Developers for latest Pixel Beta ...";
-wget -q -O PIXEL_GSI_HTML --no-check-certificate https://developer.android.com/topic/generic-system-image/releases 2>&1 || exit 1;
-grep -m1 -o 'li>.*(Beta)' PIXEL_GSI_HTML | cut -d\> -f2;
+set_random_beta() {
+    if [ "$(echo "$MODEL_LIST" | wc -l)" -ne "$(echo "$PRODUCT_LIST" | wc -l)" ]; then
+        die "MODEL_LIST and PRODUCT_LIST have different lengths."
+    fi
+    count=$(echo "$MODEL_LIST" | wc -l)
+    rand_index=$(( $$ % count ))
+    MODEL=$(echo "$MODEL_LIST" | sed -n "$((rand_index + 1))p")
+    PRODUCT=$(echo "$PRODUCT_LIST" | sed -n "$((rand_index + 1))p")
+    DEVICE=$(echo "$PRODUCT" | sed 's/_beta//')
+}
 
-BETA_REL_DATE="$(date -D '%B %e, %Y' -d "$(grep -m1 -o 'Date:.*' PIXEL_GSI_HTML | cut -d\  -f2-4)" '+%Y-%m-%d')";
-BETA_EXP_DATE="$(date -D '%s' -d "$(($(date -D '%Y-%m-%d' -d "$BETA_REL_DATE" '+%s') + 60 * 60 * 24 * 7 * 6))" '+%Y-%m-%d')";
-echo "Beta Released: $BETA_REL_DATE \
-  \nEstimated Expiry: $BETA_EXP_DATE";
+DIR="$MODPATH/autopif"
+mkdir -p "$DIR"
+cd "$DIR"
 
-RELEASE="$(grep -m1 'corresponding Google Pixel builds' PIXEL_GSI_HTML | grep -o '/versions/.*' | cut -d\/ -f3)";
-ID="$(grep -m1 -o 'Build:.*' PIXEL_GSI_HTML | cut -d\  -f2)";
-INCREMENTAL="$(grep -m1 -o "$ID-.*-" PIXEL_GSI_HTML | cut -d- -f2)";
+wget -q -O PIXEL_GSI_HTML --no-check-certificate https://developer.android.com/topic/generic-system-image/releases || exit 1
+grep -m1 -o 'li>.*(Beta)' PIXEL_GSI_HTML | cut -d\> -f2
 
-wget -q -O PIXEL_GET_HTML --no-check-certificate https://developer.android.com$(grep -m1 'corresponding Google Pixel builds' PIXEL_GSI_HTML | grep -o 'href.*' | cut -d\" -f2) 2>&1 || exit 1;
-wget -q -O PIXEL_BETA_HTML --no-check-certificate https://developer.android.com$(grep -m1 'Factory images for Google Pixel' PIXEL_GET_HTML | grep -o 'href.*' | cut -d\" -f2) 2>&1 || exit 1;
+BETA_REL_DATE="$(date -D '%B %e, %Y' -d "$(grep -m1 -o 'Date:.*' PIXEL_GSI_HTML | cut -d' ' -f2-4)" '+%Y-%m-%d')"
+BETA_EXP_DATE="$(date -D '%s' -d "$(expr $(date -D '%Y-%m-%d' -d "$BETA_REL_DATE" '+%s') + 60 \* 60 \* 24 \* 7 \* 6)" '+%Y-%m-%d')"
+echo "Beta Released: $BETA_REL_DATE"
+echo "Estimated Expiry: $BETA_EXP_DATE"
 
-MODEL_LIST="$(grep -A1 'tr id=' PIXEL_BETA_HTML | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')";
-PRODUCT_LIST="$(grep -o 'factory/.*_beta' PIXEL_BETA_HTML | cut -d\/ -f2)";
+RELEASE="$(grep -m1 'corresponding Google Pixel builds' PIXEL_GSI_HTML | grep -o '/versions/.*' | cut -d/ -f3)"
+ID="$(grep -m1 -o 'Build:.*' PIXEL_GSI_HTML | cut -d' ' -f2)"
+INCREMENTAL="$(grep -m1 -o "$ID-.*-" PIXEL_GSI_HTML | cut -d- -f2)"
 
-wget -q -O PIXEL_SECBULL_HTML --no-check-certificate https://source.android.com/docs/security/bulletin/pixel 2>&1 || exit 1;
+wget -q -O PIXEL_GET_HTML --no-check-certificate https://developer.android.com$(grep -m1 'corresponding Google Pixel builds' PIXEL_GSI_HTML | grep -o 'href.*' | cut -d\" -f2) || exit 1
+wget -q -O PIXEL_BETA_HTML --no-check-certificate https://developer.android.com$(grep -m1 'Factory images for Google Pixel' PIXEL_GET_HTML | grep -o 'href.*' | cut -d\" -f2) || exit 1
 
-SECURITY_PATCH="$(grep -A15 "$(grep -m1 -o 'Security patch level:.*' PIXEL_GSI_HTML | cut -d\  -f4-)" PIXEL_SECBULL_HTML | grep -m1 -B1 '</tr>' | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')";
+MODEL_LIST="$(grep -A1 'tr id=' PIXEL_BETA_HTML | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')"
+PRODUCT_LIST="$(grep -o 'factory/.*_beta' PIXEL_BETA_HTML | cut -d/ -f2)"
 
-case "$1" in
-  -m)
-    DEVICE="$(getprop ro.product.device)";
-    case "$PRODUCT_LIST" in
-      *${DEVICE}_beta*)
-        MODEL="$(getprop ro.product.model)";
-        PRODUCT="${DEVICE}_beta";
-      ;;
-    esac;
-  ;;
-esac;
-item "Selecting Pixel Beta device ...";
-if [ -z "$PRODUCT" ]; then
-  set_random_beta() {
-    local list_count="$(echo "$MODEL_LIST" | wc -l)";
-    local list_rand="$((RANDOM % $list_count + 1))";
-    local IFS=$'\n';
-    set -- $MODEL_LIST;
-    MODEL="$(eval echo \${$list_rand})";
-    set -- $PRODUCT_LIST;
-    PRODUCT="$(eval echo \${$list_rand})";
-    DEVICE="$(echo "$PRODUCT" | sed 's/_beta//')";
-  }
-  set_random_beta;
-fi;
-echo "$MODEL ($PRODUCT)";
+wget -q -O PIXEL_SECBULL_HTML --no-check-certificate https://source.android.com/docs/security/bulletin/pixel || exit 1
 
-item "Dumping values to minimal pif.json ...";
-cat <<EOF | tee pif.json;
+SECURITY_PATCH="$(grep -A15 "$(grep -m1 -o 'Security patch level:.*' PIXEL_GSI_HTML | cut -d' ' -f4-)" PIXEL_SECBULL_HTML | grep -m1 -B1 '</tr>' | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')"
+
+echo "- Selecting Pixel Beta device ..."
+[ -z "$PRODUCT" ] && set_random_beta
+echo "$MODEL ($PRODUCT)"
+
+sdk_version="$(getprop ro.build.version.sdk)"
+sdk_version="${sdk_version:-25}"
+echo "Device SDK version: $sdk_version"
+
+echo "- Dumping values to pif.json ..."
+cat <<EOF | tee pif.json
 {
+  "FINGERPRINT": "google/$PRODUCT/$DEVICE:$RELEASE/$ID/$INCREMENTAL:user/release-keys",
   "MANUFACTURER": "Google",
   "MODEL": "$MODEL",
-  "FINGERPRINT": "google/$PRODUCT/$DEVICE:$RELEASE/$ID/$INCREMENTAL:user/release-keys",
-  "PRODUCT": "$PRODUCT",
-  "DEVICE": "$DEVICE",
   "SECURITY_PATCH": "$SECURITY_PATCH",
-  "DEVICE_INITIAL_SDK_INT": "32"
+  "DEVICE_INITIAL_SDK_INT": $sdk_version
 }
 EOF
 
-for MIGRATE in migrate.sh /data/adb/modules/playintegrityfix/migrate.sh; do
-  [ -f "$MIGRATE" ] && break; 
-done;
-if [ -f "$MIGRATE" ]; then
-  OLDJSON=/data/adb/modules/playintegrityfix/custom.pif.json;
-  [ -f "$OLDJSON" ] && grep -qE "verboseLogs|VERBOSE_LOGS" $OLDJSON && ARGS="-a";
-  item "Converting pif.json to custom.pif.json with migrate.sh:";
-  rm -f custom.pif.json;
-  sh $MIGRATE -i $ARGS pif.json;
-  if [ -n "$ARGS" ]; then
-    grep_json() { grep -m1 "$1" $2 | cut -d\" -f4; }
-    verboseLogs=$(grep_json "VERBOSE_LOGS" $OLDJSON);
-    ADVSETTINGS="spoofBuild spoofProps spoofProvider spoofSignature verboseLogs";
-    for SETTING in $ADVSETTINGS; do
-      eval [ -z \"\$$SETTING\" ] \&\& $SETTING=$(grep_json "$SETTING" $OLDJSON);
-      eval TMPVAL=\$$SETTING;
-      [ -n "$TMPVAL" ] && sed -i "s;\($SETTING\": \"\).;\1$TMPVAL;" custom.pif.json;
-    done;
-  fi;
-  grep -q '//"\*.security_patch"' $OLDJSON && sed -i 's;"\*.security_patch";//"\*.security_patch";' custom.pif.json;
-  sed -i "s;};\n  // Beta Released: $BETA_REL_DATE\n  // Estimated Expiry: $BETA_EXP_DATE\n};" custom.pif.json;
-  cat custom.pif.json;
-fi;
+cp "$DIR/pif.json" "/data/adb/pif.json"
+echo "- new pif.json saved to /data/adb/pif.json"
 
-if [ "$DIR" = /data/adb/modules/playintegrityfix/autopif2 ]; then
-  if [ -f /data/adb/modules/playintegrityfix/migrate.sh ]; then
-    NEWNAME="custom.pif.json";
-  else
-    NEWNAME="pif.json";
-  fi;
-  if [ -f "../$NEWNAME" ]; then
-    item "Renaming old file to $NEWNAME.bak ...";
-    mv -fv ../$NEWNAME ../$NEWNAME.bak;
-  fi;
-  item "Installing new json ...";
-  cp -fv $NEWNAME ..;
-  if [ -f /data/adb/modules/playintegrityfix/killgms.sh ]; then
-    item "Killing any running GMS DroidGuard process ...";
-    sh /data/adb/modules/playintegrityfix/killgms.sh 2>&1 || true;
-  fi;
-fi;
+cd "$MODPATH"
+
+echo "- Cleaning up ..."
+rm -rf "$DIR"
+
+su -c killall com.google.android.gms.unstable || echo ""
