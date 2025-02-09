2269f63ff3d124a2bf9906ab407b9e5e8b5c9f29
chiteroman <querveros999@gmail.com>
2025-02-09 09:25:14 +0100

    Add com.android.vending to DenyList changes
    Co-authored-by: Chris Renshaw <osm0sis@outlook.com>

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,14 +1,16 @@
 MODPATH="${0%/*}"
 . "$MODPATH"/common_func.sh
 
-# Remove Play Services from Magisk DenyList when set to Enforce in normal mode
+# Remove Play Services and Play Store from Magisk DenyList when set to Enforce in normal mode
 if magisk --denylist status; then
     magisk --denylist rm com.google.android.gms
+    magisk --denylist rm com.android.vending
 else
     # Check if Shamiko is installed and whitelist feature isn't enabled
     if [ -d "/data/adb/modules/zygisk_shamiko" ] && [ ! -f "/data/adb/shamiko/whitelist" ]; then
         magisk --denylist add com.google.android.gms com.google.android.gms
         magisk --denylist add com.google.android.gms com.google.android.gms.unstable
+        magisk --denylist add com.android.vending
     fi
 fi
 
