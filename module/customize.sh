8c82063fb9427b492f10a90259449f270c74cd5a
KOWX712 <leecc0503@gmail.com>
2025-03-05 05:39:30 +0800

    installation: preserve previous setting For InjectV3 Based PIF18.8 (Cherry-picked from commit 70fd04ad6ea7e13cc773f775d9232608021e9f6a)

--- module/customize.sh
+++ module/customize.sh
@@ -60,6 +60,18 @@ if [ -d "/data/adb/modules/MagiskHidePropsConf" ]; then
     ui_print "! WARNING, MagiskHidePropsConf module may cause issues with PIF."
 fi
 
+# Preserve previous setting
+spoofConfig="spoofProvider spoofProps spoofSignature DEBUG"
+for config in $spoofConfig; do
+    grep -q "$config" "/data/adb/modules/playintegrityfix/pif.json" || continue
+    if grep -q "\"$config\": true" "/data/adb/modules/playintegrityfix/pif.json"; then
+        sed -i "s/\"$config\": .*/\"$config\": true,/" "$MODPATH/pif.json"
+    else
+        sed -i "s/\"$config\": .*/\"$config\": false,/" "$MODPATH/pif.json"
+    fi
+done
+sed -i ':a;N;$!ba;s/,\n}/\n}/g' "$MODPATH/pif.json"
+
 # Check custom fingerprint
 if [ -f "/data/adb/pif.json" ]; then
     ui_print "- Backup custom pif.json"
